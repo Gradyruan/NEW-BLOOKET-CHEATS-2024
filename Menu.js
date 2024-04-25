f26ab05c0481085e8403e17afb35ec57247f21c3
Gradyruan <124465586+Gradyruan@users.noreply.github.com>
2024-04-25 13:24:33 -0400

    Update Menu.js
    This is with the help of somebody who doesn't know it. >:)

--- Menu.js
+++ Menu.js
@@ -1,3 +1,15 @@
+/**
+* @license StewartPrivateLicense-2.0.1
+* Copyright (c) Shploop 2024
+*
+* You may not reproduce or distribute any code inside this file without the licenser's permission.
+* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
+* You may not under any circumstance republish any code from this file as your own.
+* 
+* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
+* https://github.com/Shploop/Blooket-Cheats/blob/main/LICENSE
+*/
+
 (() => {
     const cheat = (async () => {
         if (String(Function.prototype.call).includes('native')) {
